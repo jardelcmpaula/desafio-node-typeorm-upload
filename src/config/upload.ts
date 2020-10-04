47d2d4d6ca1e82c285b1f305ce3a0e6eff917e9f
Jardel Costa <jardel.costa056@gmail.com>
2020-10-03 21:57:12 -0300

    #

---
+++ src/config/upload.ts
@@ -0,0 +1,17 @@
+import path from 'path';
+import crypto from 'crypto';
+import multer from 'multer';
+
+const tmp = path.resolve(__dirname, '..', '..', 'tmp');
+export default {
+    directory: tmp,
+    // serve para armazenar os arquivos na propria aplicacao/maquina
+    storage: multer.diskStorage({
+        destination: tmp,
+        filename(request, file, callback) {
+            const fileHash = crypto.randomBytes(10).toString('HEX');
+            const fileName = `${fileHash}-${file.originalname}`;
+            return callback(null, fileName);
+        },
+    }),
+};
